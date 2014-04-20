9063cdaa742a9965b9757c9156e72b085472e993
Frank Purcell <fxpurcell@gmail.com>
2014-04-19 23:04:19 -0700

    fix loader paths ... move tests into better place...

--- ott/data/db/load.py
+++ ott/data/db/load.py
@@ -3,7 +3,7 @@ from pkg_resources import resource_filename
 from gtfsdb import Database
 from gtfsdb import Stop
 
-from ott.json.stop_response import StopResponse
+from ott.data.json.stop_response import StopResponse
 
 def get_model(db, model):
     return db.session.query(model)
@@ -12,5 +12,3 @@ def main():
     db = Database(url="sqlite:///gtfs.db")
     s = StopResponse.from_stop_id('2', db.session)
     print s
-
-
