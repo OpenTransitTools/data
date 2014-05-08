95483b1f6b4474ab35f29fbdf8fb687a2f798cab
Frank Purcell <fxpurcell@gmail.com>
2014-05-08 04:48:31 -0700

    fix up route stop

--- ott/data/dao/route_stop_dao.py
+++ ott/data/dao/route_stop_dao.py
@@ -7,6 +7,32 @@ from .stop_dao import StopListDao
 
 from gtfsdb import RouteStop
 
+class RouteStopListDao(BaseDao):
+    ''' List of RouteStopDaos for both directions of a route (or one direction if loop / uno dir)
+    '''
+    def __init__(self, rs, r):
+        super(RouteStopListDao, self).__init__()
+        self.route_stops = rs
+        self.route = r
+        self.count = len(rs)
+
+    @classmethod
+    def from_route(cls, session, route_id, direction_id=None, agency="TODO", detailed=False):
+        ''' make a StopListDao based on a route_stops object
+        '''
+        route = None
+        route_stops = []
+        dirs = [0, 1]
+        if direction_id:
+            dirs = [direction_id]
+        for d in dirs:
+            rs = RouteStopDao.from_route_direction(session, route_id, d)
+            if rs and rs.route:
+                route = rs.route
+                route_stops.append(rs)
+        ret_val = RouteStopListDao(route_stops, route)
+        return ret_val
+
 class RouteStopDao(BaseDao):
     ''' RouteStopsDao is a collection of a RouteDao, a DirectionDao and a list of StopListDao objects
         the routes_stops are defined in created table in gtfsdb (e.g., gtfsdb loading logic requires
