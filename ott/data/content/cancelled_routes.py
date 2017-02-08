2ec11352fa94b14eea8d2d076a2248634088cbdc
Frank Purcell <fxpurcell@gmail.com>
2017-02-07 17:32:51 -0800

    refactor

--- ott/data/content/cancelled_routes.py
+++ ott/data/content/cancelled_routes.py
@@ -1,4 +1,4 @@
-from ott.utils import json_utils
+from .base import Base
 
 from datetime import datetime
 from datetime import timedelta
@@ -6,57 +6,9 @@ import logging
 log = logging.getLogger(__file__)
 
 
-class CancelledRoutes(object):
+class CancelledRoutes(Base):
 
     def __init__(self, url, timeout_mins=None):
         '''
         '''
-        log.info("create an instance of {0}".format(self.__class__.__name__))
-        self.url = url
-        self.content = None
-        self.timeout = 60
-        if timeout_mins:
-            self.timeout = timeout_mins
-
-        self.last_update = datetime.now() - timedelta(minutes = (self.timeout+10))
-        self.update()
-
-    def update(self):
-        try:
-            tdiff = datetime.now() - self.last_update
-            if self.content is None or tdiff > timedelta(minutes=self.timeout):
-                log.debug("updating the content")
-                self.last_update = datetime.now()
-                c = json_utils.stream_json(self.url)
-                if c is not None:
-                    self.content = c
-        except Exception, e:
-            log.warn("couldn't update the fare content: {}".format(e))
-
-    def query(self, def_val=None):
-        ''' 
-        '''
-        ret_val = def_val
-        try:
-            self.update()
-            if self.content is not None:
-                ret_val = self.content
-        except Exception, e:
-            log.warn("content query error: {}".format(e))
-        return ret_val
-
-    def __str__(self, list_sep=","):
-        # import pdb; pdb.set_trace()
-        ret_val = ""
-        self.update()
-        if self.content:
-            if isinstance(self.content, (list, tuple)):
-                for c in self.content:
-                    if len(ret_val) > 0:
-                        ret_val = ret_val + list_sep
-                    ret_val = ret_val + str(c)
-            elif isinstance(self.content, str):
-                ret_val = self.content
-            else:
-                ret_val = str(self.content)
-        return ret_val
+        super(CancelledRoutes, self).__init__(url, timeout_mins)
