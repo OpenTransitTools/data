86ec0b97d9b2b2c969c18158698a5d6bb73a8479
Frank Purcell <fxpurcell@gmail.com>
2019-01-09 22:10:00 -0800

    refactor route dao ... change for current tables

--- ott/data/dao/route_dao.py
+++ ott/data/dao/route_dao.py
@@ -3,83 +3,12 @@ from ott.utils import date_utils
 from .alerts_dao import AlertsDao
 
 from sqlalchemy.orm import object_session
-from gtfsdb import Route
+from gtfsdb import Route, CurrentRoutes
 try: Route.make_geom_lazy()
 except: pass
 
 import logging
 log = logging.getLogger(__file__)
-
-
-class RouteListDao(BaseDao):
-    """ List of RouteDao data objects ... both list and RouteDao content ready for marshaling into JSON
-    """
-    def __init__(self, routes):
-        super(RouteListDao, self).__init__()
-        self.routes = routes
-        self.count = len(routes)
-
-    @classmethod
-    def active_routes(cls, session, agency_id=None, date=None):
-        """
-        find route list from gtfsdb based on input date (default is server date)
-        todo: move this to Routes into gtfsdb
-        todo: doesn't work if a schedule has a doughnut hole, and a route (short-term cancelled) is not be active for a period of time
-        todo: use agency_id in query
-        """
-        # step 1: grab all routes
-        routes = session.query(Route).order_by(Route.route_sort_order).all()
-
-        # step 2: filter dates based on date
-        ret_val = cls._filter_active_routes(routes, date)
-
-        return ret_val
-
-    @classmethod
-    def _filter_active_routes(cls, routes, date=None):
-        """ filter the list of gtfsdb routes from the database based on date
-            todo: move this to Routes into gtfsdb
-        """
-        # import pdb; pdb.set_trace()
-        ret_val = []
-
-        # step 1: get a valid date
-        date = date_utils.str_to_date(date)
-        if date:
-            for r in routes:
-                if r:
-                    # step 2a: filter based on begin and/or end dates
-                    if r.start_date or r.end_date:
-                        if r.start_date and r.end_date:
-                            if r.start_date <= date <= r.end_date:
-                                ret_val.append(r)
-                        elif r.start_date and r.start_date <= date:
-                            ret_val.append(r)
-                        elif r.end_date and date <= r.end_date:
-                            ret_val.append(r)
-                    else:
-                        # invalid Route. dates; can't determine active status, so just pass the route as 'active'
-                        ret_val.append(r)
-        else:
-            # step 2b: if no good input (default) date, just assign pull all routes into ret_val
-            ret_val = routes
-
-        return ret_val
-
-    @classmethod
-    def route_list(cls, session, agency="TODO", detailed=False, show_alerts=False, show_geo=False):
-        """ make a list of RouteDao objects by query to the database
-        """
-        ret_val = None
-        log.info("query Route table")
-        route_list = []
-        routes = cls.active_routes(session)
-        for r in routes:
-            rte = RouteDao.from_route_orm(route=r, agency=agency, detailed=detailed, show_alerts=show_alerts, show_geo=show_geo)
-            route_list.append(rte)
-
-        ret_val = RouteListDao(route_list)
-        return ret_val
 
 
 class RouteDao(BaseDao):
@@ -143,3 +72,78 @@ class RouteDao(BaseDao):
         log.info("query Route table")
         route = session.query(Route).filter(Route.route_id == route_id).one()
         return cls.from_route_orm(route, agency=agency, detailed=detailed, show_alerts=show_alerts, show_geo=show_geo)
+
+
+class RouteListDao(BaseDao):
+    """ List of RouteDao data objects ... both list and RouteDao content ready for marshaling into JSON
+    """
+    def __init__(self, routes):
+        super(RouteListDao, self).__init__()
+        self.routes = routes
+        self.count = len(routes)
+
+    @classmethod
+    def route_list(cls, session, agency="TODO", detailed=False, show_alerts=False, show_geo=False):
+        """ make a list of RouteDao objects by query to the database
+        """
+        ret_val = None
+        log.info("query Route table")
+        route_list = []
+        routes = cls._active_routes(session)
+        for r in routes:
+            rte = RouteDao.from_route_orm(route=r, agency=agency, detailed=detailed, show_alerts=show_alerts, show_geo=show_geo)
+            route_list.append(rte)
+
+        ret_val = RouteListDao(route_list)
+        return ret_val
+
+    @classmethod
+    def _active_routes(cls, session, agency_id=None, date=None):
+        """
+        find route list from gtfsdb based on input date (default is server date)
+        """
+        # import pdb; pdb.set_trace()
+        ret_val = []
+
+        # step 1: grab all routes
+        routes = session.query(Route).order_by(Route.route_sort_order).all()
+
+        # step 2: get a valid date
+        date = date_utils.str_to_date(date)
+        if date:
+            for r in routes:
+                if r:
+                    # step 2a: filter based on begin and/or end dates
+                    if r.start_date or r.end_date:
+                        if r.start_date and r.end_date:
+                            if r.start_date <= date <= r.end_date:
+                                ret_val.append(r)
+                        elif r.start_date and r.start_date <= date:
+                            ret_val.append(r)
+                        elif r.end_date and date <= r.end_date:
+                            ret_val.append(r)
+                    else:
+                        # invalid Route. dates; can't determine active status, so just pass the route as 'active'
+                        ret_val.append(r)
+        else:
+            # step 2b: if no good input (default) date, just assign pull all routes into ret_val
+            ret_val = routes
+
+        return ret_val
+
+
+class CurrentRoutesListDao(RouteListDao):
+    """
+    List of RouteDao data objects ... directly from the CurrentRoutes tables
+    """
+    def __init__(self, routes):
+        super(CurrentRoutesListDao, self).__init__(routes)
+
+    @classmethod
+    def _active_routes(cls, session, agency_id=None, date=None):
+        # import pdb; pdb.set_trace()
+        try:
+            ret_val = CurrentRoutes.query_routes()
+        except:
+            ret_val = super(CurrentRoutesListDao, cls)._active_routes(session, agency_id, date)
+        return ret_val
