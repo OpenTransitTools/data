e1af2ab611227ef90746afb1f765aad482d5d9f9
Frank Purcell <fxpurcell@gmail.com>
2019-01-09 16:36:20 -0800

    start adding current table to dao

--- ott/data/dao/current_route_dao.py
+++ ott/data/dao/current_route_dao.py
@@ -1,92 +1,50 @@
-from ott.utils.dao.base import BaseDao
 from ott.utils import date_utils
 from .alerts_dao import AlertsDao
+from .route_dao import RouteListDao
 
 from sqlalchemy.orm import object_session
-from gtfsdb import Route
-try: Route.make_geom_lazy()
-except: pass
+from gtfsdb import CurrentRoutes
 
 import logging
 log = logging.getLogger(__file__)
 
 
-class RouteListDao(BaseDao):
+class CurrentRoutesListDao(RouteListDao):
     """ List of RouteDao data objects ... both list and RouteDao content ready for marshaling into JSON
     """
     def __init__(self, routes):
-        super(RouteListDao, self).__init__()
-        self.routes = routes
-        self.count = len(routes)
+        super(CurrentRoutesListDao, self).__init__(routes)
+
 
     @classmethod
     def active_routes(cls, session, agency_id=None, date=None):
         """
         find route list from gtfsdb based on input date (default is server date)
-        todo: move this to Routes into gtfsdb
-        todo: doesn't work if a schedule has a doughnut hole, and a route (short-term cancelled) is not be active for a period of time
-        todo: use agency_id in query
         """
-        # step 1: grab all routes
-        routes = session.query(Route).order_by(Route.route_sort_order).all()
+        croutes = session.query(CurrentRoutes).order_by(CurrentRoutes.route_sort_order).all()
 
-        # step 2: filter dates based on date
-        ret_val = cls._filter_active_routes(routes, date)
-
-        return ret_val
-
-    @classmethod
-    def _filter_active_routes(cls, routes, date=None):
-        """ filter the list of gtfsdb routes from the database based on date
-            todo: move this to Routes into gtfsdb
-        """
-        # import pdb; pdb.set_trace()
-        ret_val = []
-
-        # step 1: get a valid date
-        date = date_utils.str_to_date(date)
-        if date:
-            for r in routes:
-                if r:
-                    # step 2a: filter based on begin and/or end dates
-                    if r.start_date or r.end_date:
-                        if r.start_date and r.end_date:
-                            if r.start_date <= date <= r.end_date:
-                                ret_val.append(r)
-                        elif r.start_date and r.start_date <= date:
-                            ret_val.append(r)
-                        elif r.end_date and date <= r.end_date:
-                            ret_val.append(r)
-                    else:
-                        # invalid Route. dates; can't determine active status, so just pass the route as 'active'
-                        ret_val.append(r)
-        else:
-            # step 2b: if no good input (default) date, just assign pull all routes into ret_val
-            ret_val = routes
-
-        return ret_val
 
     @classmethod
     def route_list(cls, session, agency="TODO", detailed=False, show_alerts=False, show_geo=False):
         """ make a list of RouteDao objects by query to the database
         """
         ret_val = None
-        log.info("query Route table")
+        log.info("query CurrentRoute table")
         route_list = []
         routes = cls.active_routes(session)
         for r in routes:
-            rte = RouteDao.from_route_orm(route=r, agency=agency, detailed=detailed, show_alerts=show_alerts, show_geo=show_geo)
+            rte = CurrentRouteDao.from_route_orm(route=r, agency=agency, detailed=detailed, show_alerts=show_alerts, show_geo=show_geo)
             route_list.append(rte)
 
         ret_val = RouteListDao(route_list)
         return ret_val
 
 
-class RouteDao(BaseDao):
+class CurrentRouteDao(BaseDao):
     """ RouteDao data object ready for marshaling into JSON
     """
     def __init__(self, route, alerts, show_geo=False):
-        super(RouteDao, self).__init__()
+        super(CurrentRouteDao, self).__init__()
         self.copy(route, show_geo)
         self.set_alerts(alerts)
 
